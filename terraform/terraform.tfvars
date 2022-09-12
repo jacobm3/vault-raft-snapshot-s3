511712a06d4aee6dd6d3b22dea89a3c512e196da
Jacob <jacobm3@gmail.com>
2022-09-12 17:53:21 -0500

    Initial commit

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,4 @@
+vpc_id   = "vpc-4f778c2a"
+hostname = "k3s"
+key_name = "acer-wsl"
+instance_type = "t4g.micro"
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "vpc_id" { type = string }
+variable "hostname" { type = string }
+variable "key_name" { type = string }
+variable "instance_type" { type = string }
