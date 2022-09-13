216b4ad93abc3b4381281e89a91e18349a756036
jacob <jacobm3@gmail.com>
2022-09-13 11:29:37 -0500

    Add bucket lifecycle configuration and object storage class.

--- vault-snapshot-s3.sh
+++ vault-snapshot-s3.sh
@@ -16,6 +16,8 @@ export VAULT_TOKEN=$( echo $loginjson | jq -r .auth.client_token )
 # Take Vault snapshot
 #
 mkdir -p $DIR
+date > ${DIR}/cron.date
+
 ts=$(date +%Y%m%d.%H%M%S.%s)
 file=${DIR}/vault.${HOSTNAME}.${ts}.snap
 
@@ -24,7 +26,8 @@ vault operator raft snapshot save $file
 #
 # Copy snapshot to s3
 #
-aws s3 cp $file s3://${BUCKET}/${HOSTNAME}/snap/vault.${HOSTNAME}.${ts}.snap >/dev/null
+aws s3 cp --storage-class STANDARD_IA \
+  $file s3://${BUCKET}/snap/${HOSTNAME}/vault.${HOSTNAME}.${ts}.snap >/dev/null
 
 #
 # Expire old local snapshots
